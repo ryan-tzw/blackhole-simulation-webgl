2367475d050536b28d1ff4c716e279d2c8307cf5
ryanteozw <ryanteozw@gmail.com>
2026-03-30 17:29:34 +0800

    refactor(rendering): update FullscreenPassMaterial to use DEFAULT_OBSERVER_CAMERA_STATE for camera properties

--- src/rendering/FullscreenPassMaterial.ts
+++ src/rendering/FullscreenPassMaterial.ts
@@ -3,15 +3,16 @@ import { Vector3 } from "three";
 import fragmentShader from "../shaders/fullscreen-pass.frag.glsl";
 import vertexShader from "../shaders/fullscreen-pass.vert.glsl";
 import { extend } from "@react-three/fiber";
+import { DEFAULT_OBSERVER_CAMERA_STATE } from "./camera-state";
 
 export const FullscreenPassMaterial = shaderMaterial(
   {
-    uCameraPos: new Vector3(0, 0, 0),
-    uCameraRight: new Vector3(1, 0, 0),
-    uCameraUp: new Vector3(0, 1, 0),
-    uCameraForward: new Vector3(0, 0, -1),
-    uFovY: 1.0,
-    uAspect: 1.0,
+    uCameraPos: new Vector3(...DEFAULT_OBSERVER_CAMERA_STATE.position),
+    uCameraRight: new Vector3(...DEFAULT_OBSERVER_CAMERA_STATE.right),
+    uCameraUp: new Vector3(...DEFAULT_OBSERVER_CAMERA_STATE.up),
+    uCameraForward: new Vector3(...DEFAULT_OBSERVER_CAMERA_STATE.forward),
+    uFovY: DEFAULT_OBSERVER_CAMERA_STATE.fovYRadians,
+    uAspect: DEFAULT_OBSERVER_CAMERA_STATE.aspect,
   },
   vertexShader,
   fragmentShader,
